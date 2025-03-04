02dd71049120fe7b242822d70707115fb38587a6
deV_inh08 <vintd2004@gmail.com>
2025-03-04 10:46:33 +0700

    declare API for Profile

--- Client/src/types/user.type.ts
+++ Client/src/types/user.type.ts
@@ -1,11 +1,14 @@
+type Role = 'User' | 'Admin'
+
 export interface User {
   _id: string
-  roles: string[]
+  roles: Role[]
   email: string
-  name: string
+  name?: string
   createdAt: string
   updatedAt: string
   phone: string
   address: string
-  date_of_birth: null
+  date_of_birth?: string // ISO 8601
+  avartar?: string
 }
