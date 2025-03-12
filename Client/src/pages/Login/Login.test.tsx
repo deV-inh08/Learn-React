7ca5f302a4d2fbbd3827aa91aa82c5f15b91a16b
deV_inh08 <vintd2004@gmail.com>
2025-03-12 12:30:13 +0700

    Enter email & password in incorrect format

--- Client/src/pages/Login/Login.test.tsx
+++ Client/src/pages/Login/Login.test.tsx
@@ -1,20 +1,41 @@
-import { describe, expect, it } from 'vitest'
-import { logScreen, renderWithRouter } from '../../utils/testUtils'
+import { beforeAll, describe, expect, it } from 'vitest'
+import { renderWithRouter } from '../../utils/testUtils'
 import { path } from '../../constants/path'
-import { screen, waitFor } from '@testing-library/react'
+import { screen, waitFor, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/vitest'
 
 describe('Test Login Page', () => {
-  it('Hiển thị require khi không nhập gì', async () => {
-    const { user } = renderWithRouter({ router: path.login })
+  // 🔥 beforeAll: Chạy 1 lần trước tất cả tests
+  beforeAll(async () => {
+    renderWithRouter({ router: path.login })
     await waitFor(() => {
-      expect(screen.queryByPlaceholderText(/Enter your email/i)).toBeInTheDocument()
-      expect(screen.queryByPlaceholderText(/Enter your password/i)).toBeInTheDocument()
+      expect(screen.getByPlaceholderText(/Enter your email/i)).toBeInTheDocument()
+      expect(screen.getByPlaceholderText(/Enter your password/i)).toBeInTheDocument()
     })
-    const submitButton = document.querySelector('form button[type="submit"]') as HTMLElement
-    user.click(submitButton)
+  })
+  it('Display required error when nothing is entered', async () => {
+    const submitButton = document.querySelector('form button[type="submit"]') as HTMLButtonElement
+    fireEvent.click(submitButton)
     expect(await screen.findByText('Email là bắt buộc !')).toBeTruthy()
     expect(await screen.findByText('Password là bắt buộc !')).toBeTruthy()
-    await logScreen()
+  })
+
+  it('Enter email and password in incorrect format', async () => {
+    const emailInput = document.querySelector('form input[type="email"]') as HTMLInputElement
+    const passwordInput = document.querySelector('form input[type="password"]') as HTMLInputElement
+    const submitButton = document.querySelector('form button[type="submit"]') as HTMLButtonElement
+    fireEvent.change(emailInput, {
+      target: {
+        value: 'test@gml'
+      }
+    })
+    fireEvent.change(passwordInput, {
+      target: {
+        value: '12'
+      }
+    })
+    fireEvent.submit(submitButton)
+    expect(await screen.findByText('Email là bắt buộc !')).toBeTruthy()
+    expect(await screen.findByText('Độ dài từ 5 - 160 ký tự')).toBeTruthy()
   })
 })
